dc6507588c20cf7deab8254169e0789b4ab17c12
ayockson2500 <ayockson2500@gmail.com>
2022-10-19 03:57:54 +0100

    array of items

---
+++ src/components/data/items.tsx
@@ -0,0 +1,38 @@
+import books from "../../assets/images/books.jpg"
+import banana from "../../assets/images/banana.jpg"
+import computer from "../../assets/images/computer.jpg"
+import car from "../../assets/images/car-img.jpg"
+import phone from "../../assets/images/iphone.jpg"
+
+export const storeItems =[
+    {
+        id: 1,
+        name: "Books",
+        price: 10.53,
+        imgUrl: books
+    },
+    {
+        id: 2,
+       name: "Banana",
+        price: 8.53,
+         imgUrl: banana
+    },
+    {
+        id: 3,
+       name: "Computer",
+        price: 20.53,
+         imgUrl: computer
+    },
+    {
+        id: 4,
+       name: "Car",
+        price: 10.53,
+         imgUrl: car
+    },
+    {
+        id: 5,
+       name: "Iphone",
+        price: 100.53,
+         imgUrl: phone
+    }
+]
