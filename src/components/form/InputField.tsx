9c0644d309ed5713537645f822bf58e9914ff581
ayockson2500 <ayockson2500@gmail.com>
2022-10-22 08:42:55 +0100

    created a resuable inputfield component

---
+++ src/components/form/InputField.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type inputFieldProps = {
+  placeholder: string;
+  value: string;
+  type: string;
+  label?: string;
+  className: string
+  onChange?: () => void;
+};
+
+const InputField = ({
+  placeholder,
+  value,
+  type,
+  label,
+  className,
+  onChange,
+}: inputFieldProps) => {
+  return (
+    <div>
+        <label>{label}</label>
+      <input placeholder={placeholder} value={value} type={type} onChange={onChange} className={className}  />
+    </div>
+  );
+};
+
+export default InputField;
