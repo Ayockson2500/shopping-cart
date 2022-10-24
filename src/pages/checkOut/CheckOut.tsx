30cb6ecbb94acdfa2de6101fbcd050d58dc47276
ayockson2500 <ayockson2500@gmail.com>
2022-10-24 15:51:38 +0100

    editted the local state of the component

--- src/pages/checkOut/CheckOut.tsx
+++ src/pages/checkOut/CheckOut.tsx
@@ -1,66 +1,86 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import InputField from "../../components/form/InputField";
-import "./checkOut.scss"
+import "./checkOut.scss";
+
 
 const CheckOut = () => {
-  const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState({
+    cardholder: "",
+    cardNumber: "",
+    month: "",
+    year: "",
+    cv: "",
+  });
 
-  
+  const handleFormSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <h1 className="text-center">Credit Card Details</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleFormSubmit}>
         <div className="input-wrapper">
-        <div>
-          <h4>Card Holder</h4>
-        <InputField
-          placeholder="Card Holder"
-          value={inputValue}
-          type="text"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
-          label="Card Holder"
-          className="form-control"
-        />
-        <InputField
-          placeholder="Card Number"
-          value={inputValue}
-          type="number"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
-          label="Card Number"
-          className="form-control"
-        />
-        </div>
-        <div>
-        <h4>Expiration Date</h4>
-        <div className="expiration-date">
-        <InputField
-          placeholder="MM"
-          value={inputValue}
-          type="text"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
-          className="form-control"
-        />
-        <InputField
-          placeholder="YY"
-          value={inputValue}
-          type="text"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
-          className="form-control"
-        />
-        </div>
-        <InputField
-          placeholder="CV"
-          value={inputValue}
-          type="text"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
-          label="cv"
-          className="form-control"
-        />
-        </div>
+          <div>
+            <h4>Card Holder</h4>
+            <InputField
+              placeholder="Card Holder"
+              value={inputValue.cardholder}
+              type="text"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setInputValue({ ...inputValue, cardholder: e.target.value })
+              }
+              label="Card Holder"
+              className="form-control"
+            />
+            <InputField
+              placeholder="Card Number"
+              value={inputValue.cardNumber}
+              type="number"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setInputValue({ ...inputValue, cardNumber: e.target.value })
+              }
+              label="Card Number"
+              className="form-control"
+            />
+          </div>
+          <div>
+            <h4>Expiration Date</h4>
+            <div className="expiration-date">
+              <InputField
+                placeholder="MM"
+                value={inputValue.month}
+                type="text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setInputValue({ ...inputValue, month: e.target.value })
+                }
+                className="form-control"
+              />
+              <InputField
+                placeholder="YY"
+                value={inputValue.year}
+                type="number"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setInputValue({ ...inputValue, year: e.target.value })
+                }
+                className="form-control"
+              />
+            </div>
+            <InputField
+              placeholder="CV"
+              value={inputValue.cv}
+              type="number"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setInputValue({ ...inputValue, cv: e.target.value })
+              }
+              label="cv"
+              className="form-control"
+            />
+          </div>
         </div>
         <div className="text-center">
-        <Button style={{width: '50%', marginTop:"20px"}}>Proceed</Button>
+          <Button style={{ width: "50%", marginTop: "20px" }}>Proceed</Button>
         </div>
       </form>
     </>
