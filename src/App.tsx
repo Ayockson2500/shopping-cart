29877e2df4616e1d24f5f47aafecb621b1ebcd55
ayockson2500 <ayockson2500@gmail.com>
2022-10-25 17:13:45 +0100

    updated the routes in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Home from './pages/home/Home';
-import { Store } from './pages/store/Store';
-import About from './pages/about/About';
-import NavBar from './components/navBar/NavBar';
-import { Container } from 'react-bootstrap';
-import { ShoppingCartProvider } from './context/ShoppingCartContext';
-import CheckOut from './pages/checkOut/CheckOut';
-import ShoppingtCart from './components/ShoppingtCart';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import { Store } from "./pages/store/Store";
+import About from "./pages/about/About";
+import NavBar from "./components/navBar/NavBar";
+import { Container } from "react-bootstrap";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import CheckOut from "./pages/checkOut/CheckOut";
+import ShoppingtCart from "./components/ShoppingtCart";
 
 function App() {
   return (
     <ShoppingCartProvider>
-    <Container className="mb-4">
-     <Router>
-      <NavBar />
-      <ShoppingtCart />
-      <Routes>
-        <Route path='/'  element={<Home />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/checkout' element={<CheckOut />} />
-      </Routes>
-     </Router>
-    </Container>
+      <Container className="mb-4">
+        <NavBar />
+        <ShoppingtCart />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/checkout" element={<CheckOut />} />
+        </Routes>
+      </Container>
     </ShoppingCartProvider>
   );
 }
